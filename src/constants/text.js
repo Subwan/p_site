305b9fc05a4ef7cddc9298f4539aa754bae63e02
Alexandr Goncharov <Subwan@gmail.com>
2018-12-14 16:04:55 +0300

    Добавление картинок, небольшие изменения стиля.

--- src/constants/text.js
+++ src/constants/text.js
@@ -1,6 +1,6 @@
 export const OPEN_PAGE_TEXT = 'ТЫ МОЛОДЕЦ';
 
 export const SECTION_OF_POWER_HEADER_TEXT = 'ТВОЁ РАЗВИТИЕ';
-export const SECTION_OF_POWER_TEXT = 'ИЛИ НЕТ';
+export const SECTION_OF_POWER_TEXT = 'Давно скатились, но лучше никого нет.';
 
-export const FOOTER_TEXT = "Любимый паблик твоей мамы";
+export const FOOTER_TEXT = 'Любимый паблик твоей мамы';
